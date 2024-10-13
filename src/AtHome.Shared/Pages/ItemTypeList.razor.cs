44bf9d1fa3b81d1c6e89e15d88c32db4d00c3292
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-10-13 02:24:56 +0200

    feat(app): add list pages for items, places and itemtypes

---
+++ src/AtHome.Shared/Pages/ItemTypeList.razor.cs
@@ -0,0 +1,25 @@
+﻿using AtHome.Shared.Errors;
+using AtHome.Shared.Models;
+using AtHome.Shared.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace AtHome.Shared.Pages;
+
+public partial class ItemTypeList : ComponentBase
+{
+    [Inject] private IItemTypeApi _iItemTypeApi { get; set; }
+
+    private ErrorOr<List<ItemType>> _data = ApiErrors.NotLoaded();
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            _data = await _iItemTypeApi.GetAllAsync();
+        }
+        catch (Exception e)
+        {
+            _data = Error.Failure("Task.Failed", e.Message);
+        }
+    }
+}
---
+++ src/AtHome.Shared/Pages/PlaceList.razor.cs
@@ -0,0 +1,25 @@
+﻿using AtHome.Shared.Errors;
+using AtHome.Shared.Models;
+using AtHome.Shared.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace AtHome.Shared.Pages;
+
+public partial class PlaceList : ComponentBase
+{
+    [Inject] private IPlaceApi _placeApi { get; set; }
+
+    private ErrorOr<List<Place>> _data = ApiErrors.NotLoaded();
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            _data = await _placeApi.GetAllAsync();
+        }
+        catch (Exception e)
+        {
+            _data = Error.Failure("Task.Failed", e.Message);
+        }
+    }
+}
